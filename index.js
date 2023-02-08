4a1ee718e69f60ca012b296b2f26ae755d021a28
Roman Zaitsev <rz@yakovlev.studio>
2023-02-09 01:00:19 +0300

    Fixes for text-decorations, mobile menu and video

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const video = document.getElementById('trailer');
 const playBtn = document.getElementById('playTrailer');
 
-const play = () => {
+const onClickPlayBtn = () => {
   video.play();
+}
+
+const play = () => {
   video.className = 'game-video-content-video__bg_playing';
   video.controls = true;
   playBtn.style.visibility = 'hidden';
@@ -11,5 +14,7 @@ const play = () => {
 const pause = () => {
   playBtn.style.visibility = 'visible';
 }
+
+video.addEventListener('play', play);
 
 video.addEventListener('pause', pause);
